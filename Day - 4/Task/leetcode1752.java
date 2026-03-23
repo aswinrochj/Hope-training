c35f7b1b94fb0a1f97d26ebdbc0ae319e8eeb243
aswinrochj <aswinroch123@gmail.com>
2026-03-23 22:53:15 +0530

    Added Day - 4 folder

---
+++ Day - 4/Task/leetcode1752.java
@@ -0,0 +1,17 @@
+public class leetcode1752 {
+
+    public boolean check(int[] nums) {
+
+        int count = 0;
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > nums[(i + 1) % n]) {
+                count++;
+            }
+        }
+
+        return count <= 1;
+    }
+    
+}
